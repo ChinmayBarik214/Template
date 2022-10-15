6bbca4cb6844e9b0fb759c88a113d2eb2faf905b
ChinmayBarik214 <89087900+ChinmayBarik214@users.noreply.github.com>
2022-10-15 02:19:09 -0700

    cache

--- authen/urls.py
+++ authen/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     path('ventfeels', views.ventfeels, name="ventfeels"),
     path('products3', views.products3, name="products3"),
     path('feedback', views.feedback, name="feedback"),
+    path('calmball', views.calmball, name="calmball"),
+
 
 
 
